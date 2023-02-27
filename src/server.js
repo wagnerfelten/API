7b83663c3e64f4b889ab0673106918d5d92e9d92
WagnerRodrigues <felten19@gmail.com>
2023-02-27 16:57:38 -0300

    criando migrations para automatização da aplicacao

--- src/server.js
+++ src/server.js
@@ -1,10 +1,32 @@
+
+require("express-async-errors");
+
+const migrationsRun = require("./database/sqlite/migrations");
 const express  = require("express");
-const routes = require("./Routes")
+const routes = require("./Routes");
+const AppError = require("./utils/AppError");
 
 const app = express();
 
+migrationsRun();
+
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error!"
+    });
+})
 
 
 const PORT = 1919;
